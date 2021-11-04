9f8851e6ab60018aa3f7504be67373bd2d070e7f
E.Z. Hart <hartez@gmail.com>
2021-11-03 21:48:42 -0600

    initial

---
+++ CustomLayoutExamples/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace CustomLayoutExamples
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
